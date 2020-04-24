47f324f38d4e62109f04fd81e161bac754193364
paulapivat <paul.apivat@gmail.com>
2020-04-24 21:20:27 +0700

    first plot success

--- R/analysis.R
+++ R/analysis.R
@@ -8,3 +8,21 @@ df <- read_csv("food_data.csv")
 
 # exploratory
 str(df)
+
+# dealing with Timestamp data
+# change chr --> POSIXlt
+strptime(df$Timestamp, "%m/%d/%Y %H:%M:%OS")
+df$Timestamp <- strptime(df$Timestamp, "%m/%d/%Y %H:%M:%OS")
+
+# must change to POSIXct format before plotting
+df$Timestamp <- as.POSIXct(df$Timestamp)
+
+# delete empty rows because 50,000+ empty rows is a pain
+# delete 233 -> 50,669
+df <- df[-c(216:50669),]
+
+# first plot success
+ggplot(data = df, mapping = aes(x=Timestamp, y=df$`ยอดผลิต (kg)`))+geom_point()
+
+# make a data dictionary
+
